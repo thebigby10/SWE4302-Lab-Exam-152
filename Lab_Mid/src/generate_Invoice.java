700cb3a33c6dd9ff4ddab6b65d9c9114a509b005
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-11-20 16:36:28 +0600

    implemeted invoice generator: some bugfixing needed

--- Lab_Mid/src/generate_Invoice.java
+++ Lab_Mid/src/generate_Invoice.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 
+import Flavors.*;
+import Toppings.*;
+import CarryMedium.*;
+
+import java.io.File;
+import java.io.IOException; 
+
 public class generate_Invoice {
     ArrayList<Icecream> icecreams;
 
@@ -8,6 +15,23 @@ public class generate_Invoice {
     }
 
     public void generate_Invoice(){
-        return;
+
+      File invoiceFile = new File("Invoice.txt");
+      if(invoiceFile.createNewFile()){
+        System.out.println("Invoice File Created");
+      }
+      ArrayList<FlavorFactory> flavors = ;
+      ArrayList<ToppingsFactory> toppings;
+      CarryFactory carry;
+
+      String invoidString = "";
+      invoiceString+="Ice Cream Shop Invoice\n";
+      for (Icecream icecream : icecreams) {
+          invoidString += icecream.get_name() + "\n";
+          invoidString += icecream.get_price() + "\n";
+          invoidString += icecream.get_total_price() + "\n";
+      }
+      System.out.println("Invoice Generated");
+      return;
     }
 }
